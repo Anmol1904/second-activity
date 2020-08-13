d4bfe56a6443f2b636878596438e76ccddeb6f73
Anmol1904 <68505469+Anmol1904@users.noreply.github.com>
2020-08-13 19:25:04 +0530

    second activity

---
+++ app/src/main/java/com/edupodiumclasses/edupodium/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.edupodiumclasses.edupodium
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity","Button was clicked")
+            Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String=etUserMessage.text.toString()
+            val intent =Intent(this,SecondActivity::class.java)
+            intent.putExtra("user_message",message)
+            startActivity(intent)
+        }
+
+    }
+}
